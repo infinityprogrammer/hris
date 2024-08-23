c04cbe6cc30f36ece6c184946908352e66ef2d41
RAFI <infinityprogrammer01@gmail.com>
2024-08-23 17:55:16 +0530

    cashflow cate

---
+++ hris/hris/doctype/cashflow_category/cashflow_category.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, RAFI and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Cashflow Category', {
+	// refresh: function(frm) {
+
+	// }
+});
