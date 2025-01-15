4140c69bb96c13786fc0adab631e5529a73e0e25
RAFI <infinityprogrammer01@gmail.com>
2025-01-15 16:25:52 +0530

    changes

--- hris/hris/doctype/employee_invoice/employee_invoice.py
+++ hris/hris/doctype/employee_invoice/employee_invoice.py
@@ -1,8 +1,11 @@
 # Copyright (c) 2024, RAFI and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+from frappe.utils import flt, cint
 from frappe.model.document import Document
 
 class EmployeeInvoice(Document):
-	pass
+	
+	def before_validate(self):
+		self.total_invoice_amount = flt(self.salary_amount)+flt(self.travel_expense)+flt(self.bonus)+flt(self.commission)
--- hris/hris/doctype/request_pay_slip/request_pay_slip.py
+++ hris/hris/doctype/request_pay_slip/request_pay_slip.py
@@ -10,3 +10,23 @@ class RequestPaySlip(Document):
 	def before_validate(self):
 		self.designation = frappe.db.get_value('Employee', self.employee, 'designation')
 		self.pay_period = getdate(self.date).strftime("%B - %Y")
+
+		if not any(row.component == "Basic Salary" for row in self.details):
+			
+			self.append("details",{
+				"component": "Basic Salary",
+				"amount": frappe.db.get_value('Employee', self.employee, 'ctc'),
+				"currency": frappe.db.get_value('Employee', self.employee, 'salary_currency')
+			})
+		
+		if not any(row.component == "Bonus" for row in self.details):
+			if frappe.db.get_value('Employee', self.employee, 'custom_bonus_gross_currency'):
+				self.append("details",{
+					"component": "Bonus",
+					"amount": frappe.db.get_value('Employee', self.employee, 'custom_bonus_gross'),
+					"currency": frappe.db.get_value('Employee', self.employee, 'custom_bonus_gross_currency')
+				})
+		
+
+		total_amount = sum(row.amount for row in self.details if row.amount)
+		self.total_salary = total_amount
