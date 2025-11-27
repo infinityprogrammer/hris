c8ae5d9d28a3d74f52da91d1ed4abddd54cb4fe6
RAFI <infinityprogrammer01@gmail.com>
2025-11-27 12:48:29 +0000

    casjflow report new

---
+++ hris/hris/report/cashflow_actual/cashflow_actual.js
@@ -0,0 +1,24 @@
+// Copyright (c) 2025, RAFI and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Cashflow Actual"] = {
+	"filters": [
+		{
+			"fieldname": "company",
+			"label": __("Company"),
+			"fieldtype": "Link",
+			"options": "Company",
+			"default": frappe.defaults.get_user_default("Company"),
+			"reqd": 1
+		},
+		{
+			"fieldname": "fiscal_year",
+			"label": __("Fiscal Year"),
+			"fieldtype": "Link",
+			"options": "Fiscal Year",
+			"reqd": 1,
+			"width": "60px"
+		},
+	]
+};
